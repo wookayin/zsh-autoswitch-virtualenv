f0cd97c5b622acc0bc6c986f95f84b3efbfa87b3
Jongwook Choi <wookayin@gmail.com>
2017-07-06 16:41:24 +0900

    Normalize whitespaces and indentation
    to 2-spaces.

--- autoswitch_virtualenv.plugin.zsh
+++ autoswitch_virtualenv.plugin.zsh
@@ -1,88 +1,88 @@
 function _maybeworkon() {
   if [[ -z "$VIRTUAL_ENV" || "$1" != "$(basename $VIRTUAL_ENV)" ]]; then
-     if [ -z "$AUTOSWITCH_SILENT" ]; then
-        printf "Switching virtualenv: %s  " $1
-     fi
+    if [ -z "$AUTOSWITCH_SILENT" ]; then
+      printf "Switching virtualenv: %s  " $1
+    fi
 
-     workon "$1"
+    workon "$1"
 
-     if [ -z "$AUTOSWITCH_SILENT" ]; then
-       # For some reason python --version writes to stderr
-       printf "[%s]\n" "$(python --version 2>&1)"
-     fi
+    if [ -z "$AUTOSWITCH_SILENT" ]; then
+      # For some reason python --version writes to stderr
+      printf "[%s]\n" "$(python --version 2>&1)"
+    fi
   fi
 }
 
 # Gives the path to the nearest parent .venv file or nothing if it gets to root
 function _check_venv_path()
 {
-    local check_dir=$1
+  local check_dir=$1
 
-    if [[ -f "${check_dir}/.venv" ]]; then
-        echo "${check_dir}/.venv"
-        return
-    else
-        if [ "$check_dir" = "/" ]; then
-            return
-        fi
-        _check_venv_path "$(dirname "$check_dir")"
+  if [[ -f "${check_dir}/.venv" ]]; then
+    echo "${check_dir}/.venv"
+    return
+  else
+    if [ "$check_dir" = "/" ]; then
+      return
     fi
+    _check_venv_path "$(dirname "$check_dir")"
+  fi
 }
 
 
 # Automatically switch virtualenv when .venv file detected
 function _check_venv()
 {
-    if [ "$PWD" != "$MYOLDPWD" ]; then
-        MYOLDPWD="$PWD"
+  if [ "$PWD" != "$MYOLDPWD" ]; then
+    MYOLDPWD="$PWD"
 
-        SWITCH_TO=""
+    SWITCH_TO=""
 
-        # Get the .venv file, scanning parent directories
-        venv_path=$(_check_venv_path "$PWD")
-        if [[ -n "$venv_path" ]]; then
+    # Get the .venv file, scanning parent directories
+    venv_path=$(_check_venv_path "$PWD")
+    if [[ -n "$venv_path" ]]; then
 
-          stat --version &> /dev/null
-          if [[ $? -eq 0 ]]; then   # Linux, or GNU stat
-            file_owner="$(stat -c %u "$venv_path")"
-            file_permissions="$(stat -c %a "$venv_path")"
-          else                      # macOS, or FreeBSD stat
-            file_owner="$(stat -f %u "$venv_path")"
-            file_permissions="$(stat -f %OLp "$venv_path")"
-          fi
+      stat --version &> /dev/null
+      if [[ $? -eq 0 ]]; then   # Linux, or GNU stat
+        file_owner="$(stat -c %u "$venv_path")"
+        file_permissions="$(stat -c %a "$venv_path")"
+      else                      # macOS, or FreeBSD stat
+        file_owner="$(stat -f %u "$venv_path")"
+        file_permissions="$(stat -f %OLp "$venv_path")"
+      fi
 
-          if [[ "$file_owner" != "$(id -u)" ]]; then
-            echo "AUTOSWITCH WARNING: Virtualenv will not be activated"
-            echo ""
-            echo "Reason: Found a .venv file but it is not owned by the current user"
-            echo "Change ownership of $venv_path to '$USER' to fix this"
-          elif [[ "$file_permissions" != "600" ]]; then
-            echo "AUTOSWITCH WARNING: Virtualenv will not be activated"
-            echo ""
-            echo "Reason: Found a .venv file with weak permission settings ($file_permissions)."
-            echo "Run the following command to fix this: \"chmod 600 .venv\""
-          else
-            SWITCH_TO="$(<"$venv_path")"
-            AUTOSWITCH_PROJECT="$PWD"
-          fi
-        fi
-
-        if [[ -n "$SWITCH_TO" ]]; then
-          _maybeworkon "$SWITCH_TO"
-        elif [[ "$PWD" != "$AUTOSWITCH_PROJECT/"* ]]; then
-          _default_venv
-          AUTOSWITCH_PROJECT=""
-        fi
+      if [[ "$file_owner" != "$(id -u)" ]]; then
+        echo "AUTOSWITCH WARNING: Virtualenv will not be activated"
+        echo ""
+        echo "Reason: Found a .venv file but it is not owned by the current user"
+        echo "Change ownership of $venv_path to '$USER' to fix this"
+      elif [[ "$file_permissions" != "600" ]]; then
+        echo "AUTOSWITCH WARNING: Virtualenv will not be activated"
+        echo ""
+        echo "Reason: Found a .venv file with weak permission settings ($file_permissions)."
+        echo "Run the following command to fix this: \"chmod 600 .venv\""
+      else
+        SWITCH_TO="$(<"$venv_path")"
+        AUTOSWITCH_PROJECT="$PWD"
+      fi
     fi
+
+    if [[ -n "$SWITCH_TO" ]]; then
+      _maybeworkon "$SWITCH_TO"
+    elif [[ "$PWD" != "$AUTOSWITCH_PROJECT/"* ]]; then
+      _default_venv
+      AUTOSWITCH_PROJECT=""
+    fi
+  fi
 }
 
 # Switch to the default virtual environment
 function _default_venv()
 {
   if [[ -n "$AUTOSWITCH_DEFAULTENV" ]]; then
-     _maybeworkon "$AUTOSWITCH_DEFAULTENV"
+    _maybeworkon "$AUTOSWITCH_DEFAULTENV"
   elif [[ -n "$VIRTUAL_ENV" ]]; then
-     deactivate
+    deactivate
   fi
 }
 
